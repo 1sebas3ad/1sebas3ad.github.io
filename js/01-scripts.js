c26fa67c6ab99acbde33201f5bc760b128b59fd1
Sebas <sebastianyu_12@hotmail.com>
2023-01-11 15:00:07 -0500

    Initial

---
+++ js/01-scripts.js
@@ -0,0 +1,9 @@
+let elemento;
+
+elemento = document;
+elemento = document.all;
+elemento = document.head;
+elemento = document.body;
+
+
+console.log(elemento)
---
+++ js/06-scripts.js
@@ -0,0 +1,13 @@
+const encabezado = document.querySelector('.contenido-hero h1').textContent;
+console.log(encabezado);
+
+
+// console.log(encabezado.innerText);
+// console.log(encabezado.textContent);
+// console.log(encabezado.innerHTML);
+
+document.querySelector('.contenido-hero h1').textContent = 'Holi, esta pagina es para ti, talvez no es la gran cosa pero espero que te guste'
+console.log(encabezado);
+
+// const imagen = document.querySelector('.card img');
+// imagen.src = 'img/hacer2.jpg'
